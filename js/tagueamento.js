c36a9cef497f094d9b2389100d9a2ee43ff36b1e
yuridias04 <yuri.dias@raccoon.ag>
2021-10-15 16:28:16 -0300

    First Commit

---
+++ js/tagueamento.js
@@ -0,0 +1,11 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJZDW45');
